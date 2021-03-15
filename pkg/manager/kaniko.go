a26918c55d4017dd092a1da094caf988530196fc
spiarh <_me@spiarh.fr>
2021-03-15 01:20:29 +0100

    fix tag-latest for Kaniko
    Signed-off-by: lcavajani <lcavajani@suse.com>

--- pkg/manager/kaniko.go
+++ pkg/manager/kaniko.go
@@ -40,27 +40,18 @@ func (k *Kaniko) Build(build *core.Build) error {
 	for arg, val := range buildArgs {
 		task.AddArgs("--build-arg", fmt.Sprintf("%s=%s", arg, val))
 	}
-	// reuse local cache so we can repush with new tag, this is
-	// to avoid creating the tar and using a second tool to push different tags.
-	// TODO: check... not sure this works
-	task.AddArgs("--cache=true")
 	task.AddArgs("--context", build.Image.Context)
 	task.AddArgs("--dockerfile", build.Image.Containerfile)
 	task.AddArgs("--destination", build.Image.String())
+
+	if k.tagLatest {
+		task.AddArgs("--destination", build.Image.StringWithTagLatest())
+	}
 
 	_, err := task.Execute()
 	if err != nil {
 		return err
 	}
 
-	if k.tagLatest {
-		// rerun with a new tag
-		task.Args = task.Args[:len(task.Args)-1]
-		task.AddArgs(build.Image.StringWithTagLatest())
-		_, err := task.Execute()
-		if err != nil {
-			return err
-		}
-	}
 	return nil
 }
